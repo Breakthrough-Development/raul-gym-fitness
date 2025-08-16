caf3338405cf12b266d83c22e113f022806fdd93
Royer Adames <royeraadames@gmail.com>
2025-08-16 19:00:23 -0400

    Add ticket status icons to TicketsPage for improved visual representation of ticket states

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,6 +4,62 @@ import { ticketPath } from "@/paths";
 import type { Route } from "next";
 import clsx from "clsx";
 
+const PencilIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m4.5 12.75 6 6 9-13.5"
+    />
+  </svg>
+);
+const DocumentIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+    />
+  </svg>
+);
+
+const CheckIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m4.5 12.75 6 6 9-13.5"
+    />
+  </svg>
+);
+
+const TICKET_ICONS = {
+  OPEN: <DocumentIcon />,
+  IN_PROGRESS: <PencilIcon />,
+  DONE: <CheckIcon />,
+};
+
 export default function TicketsPage() {
   return (
     <section className="flex-1 flex flex-col gap-y-8">
@@ -20,6 +76,7 @@ export default function TicketsPage() {
               key={ticket.id}
               className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
             >
+              <div>{TICKET_ICONS[ticket.status]}</div>
               <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
               <p
                 className={clsx("text-sm text-slate-500 truncate", {
