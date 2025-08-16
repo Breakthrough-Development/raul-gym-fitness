d551f95fca0d5a8fa154f307a0632fc3ddd4d241
Royer Adames <royeraadames@gmail.com>
2025-08-16 16:45:23 -0400

    Revamp TicketsPage layout with enhanced styling and improved ticket display, including title and content. Update structure to use semantic HTML elements for better accessibility.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -5,17 +5,31 @@ import type { Route } from "next";
 
 export default function TicketsPage() {
   return (
-    <div>
-      <ul>
-        {initialTickets.map((ticket) => (
-          <li key={ticket.id}>
-            <h2>{ticket.title}</h2>
-            <Link href={ticketPath(ticket.id.toString()) as Route}>
-              View <span className="sr-only">ticket {ticket.id}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <section className="flex-1 flex flex-col gap-y-8">
+      <header>
+        <h2 className="text-3xl font-bold tracking-tight">Tickets Page</h2>
+        <p className="text-sm text-muted-foreground">
+          All your tickets at one place.
+        </p>
+      </header>
+      <div>
+        <ul className="flex-1 flex flex-col items-center gap-y-4">
+          {initialTickets.map((ticket) => (
+            <li
+              key={ticket.id}
+              className="w-full max-w-[420px] p-4 border border-slate-100 rounded"
+            >
+              <h3 className="text-lg font-semibold truncate">{ticket.title}</h3>
+              <p className="text-sm text-slate-500 truncate">
+                {ticket.content}
+              </p>
+              <Link href={ticketPath(ticket.id.toString()) as Route}>
+                View <span className="sr-only">ticket {ticket.id}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 }
