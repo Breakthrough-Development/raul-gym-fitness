ce44f3ac1f95854fdd1503f66fd5d053a4e7ce05
Royer Adames <royeraadames@gmail.com>
2025-08-17 13:20:40 -0400

    Implement Placeholder component for improved error handling in TicketPage; refactor ticket not found message to utilize the new component for better UI consistency.

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -2,6 +2,9 @@ import initialTickets from "@/tickets.data";
 import Link from "next/link";
 import { ticketsPath } from "@/paths";
 import type { Route } from "next";
+import Placeholder from "@/components/placeholder";
+import { Button } from "@/components/ui/button";
+
 export type TicketPageProps = {
   params: {
     ticketId: string;
@@ -13,10 +16,14 @@ export default function TicketPage({ params }: TicketPageProps) {
   const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
   if (!ticket) {
     return (
-      <section>
-        <h1>Ticket not found</h1>
-        <Link href={ticketsPath() as Route}>Back to tickets page</Link>
-      </section>
+      <Placeholder
+        label="Ticket not found"
+        button={
+          <Button asChild variant="outline">
+            <Link href={ticketsPath() as Route}>Back to tickets</Link>
+          </Button>
+        }
+      />
     );
   }
   return (
