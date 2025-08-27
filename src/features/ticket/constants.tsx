d279b0ccbfbc08783a2c6f67e3138cc7ac55d0fc
Royer Adames <royeraadames@gmail.com>
2025-08-27 05:11:23 -0400

    Remove unused ticket example data and constants from constants.tsx, streamlining the codebase and improving maintainability.

--- src/features/ticket/constants.tsx
+++ src/features/ticket/constants.tsx
@@ -1,22 +1,4 @@
-import type { Ticket } from "./types";
 import { LucideCircleCheck, LucideFileText, LucidePencil } from "lucide-react";
-
-export const TICKET_STATUSES = ["OPEN", "IN_PROGRESS", "DONE"] as const;
-
-export const EXAMPLE_TICKETS: Ticket[] = [
-  {
-    id: 1,
-    title: "Ticket 1",
-    content: "This is the first ticket",
-    status: "DONE",
-  },
-  {
-    id: 2,
-    title: "Ticket 2",
-    content: "This is the second ticket",
-    status: "OPEN",
-  },
-];
 
 export const TICKET_ICONS = {
   OPEN: <LucideFileText />,
