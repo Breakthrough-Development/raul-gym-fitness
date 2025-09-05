ba42fadc13d9b912b6e6085cc9b7b120052cc9ea
Royer Adames <royeraadames@gmail.com>
2025-09-05 05:51:42 -0400

    Implement ticket creation logic in createTicket action, defining data structure for new tickets. This sets the groundwork for integrating ticket management functionality.

--- src/features/ticket/actions/create-ticket.tsx
+++ src/features/ticket/actions/create-ticket.tsx
@@ -5,13 +5,11 @@ import { ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
 
 const createTicket = async (formData: FormData) => {
-  // todo: implement ticket creation
   const data = {
     title: formData.get("title") as string,
     content: formData.get("content") as string,
     status: "OPEN",
   };
-  console.log(data);
 
   await prisma.ticket.create({
     data: {
