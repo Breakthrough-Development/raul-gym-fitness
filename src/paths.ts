5cd32572731e362beaf025dab6bf2ff2687b8b87
Royer Adames <royeraadames@gmail.com>
2025-09-05 05:51:31 -0400

    Enhance path management for tickets by introducing ticketEditPath and refactoring ticketPath to utilize ticketsPath. This improves code maintainability and consistency in URL structure.

--- src/paths.ts
+++ src/paths.ts
@@ -1,5 +1,8 @@
 import type { Route } from "next";
 
-export const ticketsPath = () => "/tickets" as Route    ;
-export const ticketPath = (ticketId: string) => `/tickets/${ticketId}` as Route;
+export const ticketsPath = () => "/tickets" as Route;
+export const ticketPath = (ticketId: string) =>
+  `${ticketsPath()}/${ticketId}` as Route;
+export const ticketEditPath = (ticketId: string) =>
+  `${ticketsPath()}/${ticketId}/edit` as Route;
 export const homePath = () => "/" as Route;
