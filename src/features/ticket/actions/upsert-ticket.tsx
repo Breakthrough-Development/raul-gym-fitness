ebd6f2511d44bd3281fa0125c7b4767bf4edf30d
Royer Adames <royeraadames@gmail.com>
2025-09-06 13:14:25 -0400

    Refactor upsertTicket function to improve error handling and redirect logic. The function now returns structured feedback with fieldErrors and correctly handles ticket creation and updates, enhancing user experience during form submissions.

--- src/features/ticket/actions/upsert-ticket.tsx
+++ src/features/ticket/actions/upsert-ticket.tsx
@@ -5,7 +5,10 @@ import { ticketPath, ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import z from "zod";
-import { ActionState, formErrorToActionState } from "@/components/form/to-action-state";
+import {
+  ActionState,
+  formErrorToActionState,
+} from "@/components/form/util/to-action-state";
 
 const upsertTicketSchema = z.object({
   title: z.string().min(1).max(191),
@@ -16,7 +19,7 @@ const upsertTicket = async (
   id: string | undefined,
   _actionState: ActionState,
   formData: FormData
-) => {
+): Promise<ActionState> => {
   try {
     const data = upsertTicketSchema.parse({
       title: formData.get("title") as string,
@@ -31,13 +34,13 @@ const upsertTicket = async (
   } catch (error) {
     return formErrorToActionState(error, formData);
   }
+
   revalidatePath(ticketsPath());
 
-  if (!id) {
-    return { message: "Ticket created" };
+  if (id) {
+    redirect(ticketPath(id));
   }
-  redirect(ticketPath(id));
-  return { message: "Ticket updated" };
+  return { message: "Ticket updated", fieldErrors: {} };
 };
 
 export { upsertTicket };
