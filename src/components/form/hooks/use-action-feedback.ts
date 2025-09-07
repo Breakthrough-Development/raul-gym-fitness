6a4a1ffa7ac31c41782728350d3b78cb994fe0b2
Royer Adames <royeraadames@gmail.com>
2025-09-07 10:28:31 -0400

    Enhance useActionFeedback hook to track action state updates using a ref. This change improves the hook's efficiency by preventing unnecessary effect executions when the action state timestamp remains unchanged, ensuring more accurate feedback handling during form submissions.

--- src/components/form/hooks/use-action-feedback.ts
+++ src/components/form/hooks/use-action-feedback.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { ActionState } from "../util/to-action-state";
 
 type OnArgs = {
@@ -13,14 +13,19 @@ const useActionFeedback = (
   actionState: ActionState,
   options: UseActionFeedbackOptions
 ) => {
+  const prevTimestamp = useRef(actionState.timestamp);
+  const isUpdate = prevTimestamp.current !== actionState.timestamp;
   useEffect(() => {
+    if (!isUpdate) return;
     if (actionState.status === "SUCCESS") {
       options.onSuccess?.({ actionState });
     }
     if (actionState.status === "ERROR") {
       options.onError?.({ actionState });
     }
-  }, [actionState, options]);
+
+    prevTimestamp.current = actionState.timestamp;
+  }, [actionState, options, isUpdate]);
 };
 
 export { useActionFeedback };
