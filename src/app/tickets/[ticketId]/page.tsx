00140709b9fbda9514a744dcee3bd4fa92573b5f
Royer Adames <royeraadames@gmail.com>
2025-09-12 09:14:57 -0400

    Refactor TicketsPage and TicketPage components to remove RedirectToast. This change simplifies the structure of both pages while maintaining functionality, enhancing readability and user experience.

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,4 +1,3 @@
-import { RedirectToast } from "@/components/redirect-toast";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 import { notFound } from "next/navigation";
@@ -16,11 +15,8 @@ export default async function TicketPage({ params }: TicketPageProps) {
     return notFound();
   }
   return (
-    <>
-      <div className="flex justify-center animate-fade-from-top">
-        <TicketItem ticket={ticket} isDetail />
-      </div>
-      <RedirectToast />
-    </>
+    <div className="flex justify-center animate-fade-from-top">
+      <TicketItem ticket={ticket} isDetail />
+    </div>
   );
 }
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -6,31 +6,27 @@ import { ErrorBoundary } from "react-error-boundary";
 import Placeholder from "@/components/placeholder";
 import { CardComp } from "@/components/card-comp";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
-import { RedirectToast } from "@/components/redirect-toast";
 
 export default async function TicketsPage() {
   return (
-    <>
-      <section className="flex-1 flex flex-col gap-y-8">
-        <Heading
-          title="Tickets Page"
-          description="All your tickets at one place."
-        />
+    <section className="flex-1 flex flex-col gap-y-8">
+      <Heading
+        title="Tickets Page"
+        description="All your tickets at one place."
+      />
 
-        <CardComp
-          title="Tickets"
-          description="All your tickets at one place."
-          content={<TicketUpsertForm />}
-          className="w-full max-w-[420px] self-center"
-        ></CardComp>
+      <CardComp
+        title="Tickets"
+        description="All your tickets at one place."
+        content={<TicketUpsertForm />}
+        className="w-full max-w-[420px] self-center"
+      ></CardComp>
 
-        <ErrorBoundary fallback={<Placeholder label="Error loading tickets" />}>
-          <Suspense fallback={<Spinner />}>
-            <TicketList />
-          </Suspense>
-        </ErrorBoundary>
-      </section>
-      <RedirectToast />
-    </>
+      <ErrorBoundary fallback={<Placeholder label="Error loading tickets" />}>
+        <Suspense fallback={<Spinner />}>
+          <TicketList />
+        </Suspense>
+      </ErrorBoundary>
+    </section>
   );
 }
