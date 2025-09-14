928d654c2f2737be786a94a874390bf657026ba0
Royer Adames <royeraadames@gmail.com>
2025-09-14 15:12:22 -0400

    Add ConfirmDialog component for user confirmation interactions. Integrates alert dialog UI elements to enhance user experience during critical actions.

---
+++ src/components/confirm-dialog.tsx
@@ -0,0 +1,46 @@
+import {
+  AlertDialogAction,
+  AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "./ui/alert-dialog";
+import { Button } from "./ui/button";
+
+type ConfirmDialogProps = {
+  title?: string;
+  description?: string;
+  action: (payload: FormData) => void;
+  trigger: React.ReactElement;
+};
+
+const ConfirmDialog = ({
+  title,
+  description,
+  action,
+  trigger,
+}: ConfirmDialogProps) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{trigger}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogAction asChild>
+            <form action={action}>
+              <Button type="submit">Confirm</Button>
+            </form>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export { ConfirmDialog };
