1161cd82600f0606973be11d80ea89a9f3a87de8
Royer Adames <royeraadames@gmail.com>
2025-09-19 16:14:11 -0400

    Implement error handling in deleteTicket function to manage deletion failures. This update enhances robustness by returning a structured error state when ticket deletion encounters issues.

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -4,13 +4,18 @@ import { ticketsPath } from "@/paths";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { setCookieByKey } from "@/actions/cookies";
+import { formErrorToActionState } from "@/components/form/util/to-action-state";
 
 export const deleteTicket = async (id: string) => {
-  await prisma.ticket.delete({
-    where: {
-      id,
-    },
-  });
+  try {
+    await prisma.ticket.delete({
+      where: {
+        id,
+      },
+    });
+  } catch (error) {
+    return formErrorToActionState(error);
+  }
 
   revalidatePath(ticketsPath());
   await setCookieByKey("toast", "Ticket deleted");
