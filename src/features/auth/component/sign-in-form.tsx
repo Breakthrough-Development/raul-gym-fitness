67e8a8f46d54f7b3a7e90fabc82dfe8099c5e03f
Royer Adames <royeraadames@gmail.com>
2025-09-20 19:32:42 -0400

    Add SignInForm component for user authentication with email and password input fields, including error handling.

---
+++ src/features/auth/component/sign-in-form.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { signIn } from "../actions/sign-in";
+import { useActionState } from "react";
+import { EMPTY_ACTION_STATE } from "@/components/form/util/to-action-state";
+import { Form } from "@/components/form/form";
+import { SubmitButton } from "@/components/form/submit-button";
+import { FieldError } from "@/components/form/field-error";
+import { Input } from "@/components/ui/input";
+
+const SignInForm = () => {
+  const [actionState, action] = useActionState(signIn, EMPTY_ACTION_STATE);
+
+  return (
+    <Form action={action} actionState={actionState}>
+      <Input name="email" placeholder="Email" />
+      <FieldError actionState={actionState} name="email" />
+
+      <Input type="password" name="password" placeholder="Password" />
+      <FieldError actionState={actionState} name="password" />
+
+      <SubmitButton label="Sign In" />
+    </Form>
+  );
+};
+
+export { SignInForm };
