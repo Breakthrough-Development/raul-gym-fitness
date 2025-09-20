c3af5eed3d1e08d6ecd922af9b17a62fbdcd5bc1
Royer Adames <royeraadames@gmail.com>
2025-09-20 13:49:32 -0400

    Add sign-up, sign-in, and password forgot paths to paths.ts for user authentication flow

--- src/paths.ts
+++ src/paths.ts
@@ -6,3 +6,6 @@ export const ticketPath = (ticketId: string) =>
 export const ticketEditPath = (ticketId: string) =>
   `${ticketsPath()}/${ticketId}/edit` as Route;
 export const homePath = () => "/" as Route;
+export const signUpPath = () => "/sign-up" as Route;
+export const signInPath = () => "/sign-in" as Route;
+export const passwordForgotPath = () => "/password-forgot" as Route;
