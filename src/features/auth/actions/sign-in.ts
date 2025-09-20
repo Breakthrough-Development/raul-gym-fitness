e32d7bc5143280b7f104a03efcc9f5a9d4bf1816
Royer Adames <royeraadames@gmail.com>
2025-09-20 19:32:32 -0400

    Add sign-in action with validation and user authentication logic using Zod and Prisma

---
+++ src/features/auth/actions/sign-in.ts
@@ -0,0 +1,58 @@
+"use server";
+
+import {
+  ActionState,
+  formErrorToActionState,
+  toActionState,
+} from "@/components/form/util/to-action-state";
+import z from "zod";
+import { prisma } from "@/lib/prisma";
+import { verify } from "@node-rs/argon2";
+import { lucia } from "@/lib/lucia";
+import { cookies } from "next/headers";
+import { ticketsPath } from "@/paths";
+import { redirect } from "next/navigation";
+
+const signInSchema = z.object({
+  email: z.email().min(1, { message: "Is required" }).max(191),
+  password: z.string().min(6).max(191),
+});
+
+const signIn = async (_actionState: ActionState, formData: FormData) => {
+  try {
+    const { email, password } = signInSchema.parse(
+      Object.fromEntries(formData)
+    );
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return toActionState("ERROR", "Incorrect email or password");
+    }
+
+    const validPassword = await verify(user.password, password);
+
+    if (!validPassword) {
+      return toActionState("ERROR", "Incorrect email or password");
+    }
+
+    const session = await lucia.createSession(user.id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+
+    (await cookies()).set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+  } catch (error) {
+    return formErrorToActionState(error, formData);
+  }
+
+  redirect(ticketsPath());
+};
+
+export { signIn };
