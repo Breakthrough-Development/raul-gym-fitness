42ef42a471c963eedf99b8463793c57f199d8e1f
Royer Adames <royeraadames@gmail.com>
2025-09-21 12:35:12 -0400

    Enhance SubmitButton component by adding optional icon, variant, and size props for improved customization and flexibility in form submissions.

--- src/components/form/submit-button.tsx
+++ src/components/form/submit-button.tsx
@@ -2,17 +2,45 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "../ui/button";
 import { LucideLoaderCircle } from "lucide-react";
+import clsx from "clsx";
+import { cloneElement } from "react";
 
 export type SubmitButtonProps = {
-  label: string;
+  label?: string;
+  icon?: React.ReactElement<{ className?: string }>;
+  variant?:
+    | "default"
+    | "outline"
+    | "secondary"
+    | "destructive"
+    | "ghost"
+    | "link";
+  size?: "sm" | "default" | "lg" | "icon";
 };
 
-const SubmitButton = ({ label }: SubmitButtonProps) => {
+const SubmitButton = ({
+  label,
+  icon,
+  variant = "default",
+  size = "default",
+}: SubmitButtonProps) => {
   const { pending } = useFormStatus();
+  const iconElement = icon
+    ? cloneElement(icon, {
+        className: clsx("h-4 w-4", icon.props.className),
+      })
+    : null;
   return (
-    <Button type="submit" disabled={pending}>
-      {pending && <LucideLoaderCircle className="h-4 w-4 animate-spin" />}
+    <Button type="submit" disabled={pending} variant={variant} size={size}>
+      {pending && (
+        <LucideLoaderCircle
+          className={clsx("h-4 w-4 animate-spin", {
+            "mr-2": !!label,
+          })}
+        />
+      )}
       {label}
+      {pending ? null : iconElement}
     </Button>
   );
 };
