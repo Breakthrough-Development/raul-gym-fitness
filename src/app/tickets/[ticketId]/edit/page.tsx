5e3b6ee266b5ffd76467e330880a97a620514788
Royer Adames <royeraadames@gmail.com>
2025-09-23 17:17:01 -0400

    show not found page if the user doesn't make owner user id

--- src/app/tickets/[ticketId]/edit/page.tsx
+++ src/app/tickets/[ticketId]/edit/page.tsx
@@ -1,4 +1,6 @@
 import { CardComp } from "@/components/card-comp";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+import { isOwner } from "@/features/auth/utils/is-owner";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
 import { notFound } from "next/navigation";
@@ -8,11 +10,16 @@ export type TicketEditPageProps = {
 };
 
 const TicketEditPage = async ({ params }: TicketEditPageProps) => {
+  const { user } = await getAuthOrRedirect();
   const { ticketId } = await params;
   const ticket = await getTicket(ticketId);
-  if (!ticket) {
+
+  const isTicketFound = !!ticket;
+  const isTicketOwner = isOwner(user, ticket);
+  if (!isTicketFound || !isTicketOwner) {
     return notFound();
   }
+
   return (
     <div className="flex-1 flex flex-col justify-center items-center">
       <CardComp
