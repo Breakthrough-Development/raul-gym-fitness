41c3412ff000447cbea0cdea99145d34ab186c3e
Royer Adames <royeraadames@gmail.com>
2025-09-23 11:08:39 -0400

    replace lucia User type with prisma User type

--- src/features/auth/hooks/use-auth.ts
+++ src/features/auth/hooks/use-auth.ts
@@ -1,11 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
 import { getAuth } from "../queries/get-auth";
-import { User as AuthUser } from "lucia";
+import { User } from "@prisma/client";
 import { usePathname } from "next/navigation";
 
 const useAuth = () => {
-  const [user, setUser] = useState<AuthUser | null>(null);
+  const [user, setUser] = useState<User | null>(null);
   const [isFetched, setIsFetched] = useState(false);
 
   const pathname = usePathname();
