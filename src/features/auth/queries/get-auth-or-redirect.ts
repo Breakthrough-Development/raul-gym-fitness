eafe23afab703e38ce67a0d0e6dbd0841cec4d6e
Royer Adames <royeraadames@gmail.com>
2025-09-23 17:04:40 -0400

    Add getAuthOrRedirect function to handle authentication and redirect to sign-in path if user is not authenticated

---
+++ src/features/auth/queries/get-auth-or-redirect.ts
@@ -0,0 +1,13 @@
+import { signInPath } from "@/paths";
+import { getAuth } from "./get-auth";
+import { redirect } from "next/navigation";
+
+export const getAuthOrRedirect = async () => {
+  const auth = await getAuth();
+
+  if (!auth.user) {
+    redirect(signInPath());
+  }
+
+  return auth;
+};
