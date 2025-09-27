a15454839a3b5fc7dbc3a06430978a2380ae1b7a
Royer Adames <royeraadames@gmail.com>
2025-09-27 14:40:46 -0400

    Replace ticket navigation link with TicketList component wrapped in Suspense for improved loading experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import Heading from "@/components/heading";
-import { ticketsPath } from "@/paths";
-import Link from "next/link";
+import { Spinner } from "@/components/spiner";
+import TicketList from "@/features/ticket/components/ticket-list";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Home" description="Your home place to start" />
 
-      <div className="flex-1 flex flex-col items-center">
-        <Link href={ticketsPath()} className="text-sm underline">
-          Go to Tickets
-        </Link>
-      </div>
+      <Suspense fallback={<Spinner />}>
+        <TicketList />
+      </Suspense>
     </div>
   );
 }
