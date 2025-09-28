c7d571be24bd1b2335041a97b56fdaca33774c61
Royer Adames <royeraadames@gmail.com>
2025-09-27 23:39:29 -0400

    breadcrumbs in ticket page

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,5 +1,8 @@
+import { Breadcrumbs } from "@/components/breadcrumbs";
+import { Separator } from "@/components/ui/separator";
 import TicketItem from "@/features/ticket/components/ticket-item";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { ticketPath, ticketsPath } from "@/paths";
 import { notFound } from "next/navigation";
 
 export type TicketPageProps = {
@@ -15,8 +18,19 @@ export default async function TicketPage({ params }: TicketPageProps) {
     return notFound();
   }
   return (
-    <div className="flex justify-center animate-fade-from-top">
-      <TicketItem ticket={ticket} isDetail />
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Breadcrumbs
+        breadCrumbs={[
+          { title: "Tickets", href: ticketsPath() },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+        ]}
+      />
+
+      <Separator />
+
+      <div className="flex justify-center animate-fade-from-top">
+        <TicketItem ticket={ticket} isDetail />
+      </div>
     </div>
   );
 }
