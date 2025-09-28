03fcf0c407ec292f84e4d4e488a61a7f8c418f36
Royer Adames <royeraadames@gmail.com>
2025-09-27 23:37:29 -0400

    Enhance main layout with sidebar animation and padding adjustments for improved user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
           <Header />
           <div className="flex h-screen overflow-hidden border-collapse">
             <Sidebar />
-            <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
+            <main className="duration-200 pl-[100px] peer-hover:pl-[240px] min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
               {children}
             </main>
           </div>
