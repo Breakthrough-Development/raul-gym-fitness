3a3ae1bd91e76136bf219dfff4a33642aef28e09
Royer Adames <royeraadames@gmail.com>
2025-09-29 19:54:46 -0400

    place update form on page

--- src/app/account/password/page.tsx
+++ src/app/account/password/page.tsx
@@ -1,15 +1,19 @@
 import Heading from "@/components/heading";
 import { AccountTabs } from "@/features/account/components/account-tabs";
+import { ResetPasswordForm } from "@/features/auth/component/update-password-form";
 
 const passwordPage = () => {
   return (
-    <div className="flex-1 flex flex-col gap-y-8">
+    <section className="flex-1 flex flex-col gap-y-8">
       <Heading
         title="Password"
         description="Change your password"
         tabs={<AccountTabs />}
       />
-    </div>
+      <div className="w-full max-w-[420px] self-center">
+        <ResetPasswordForm />
+      </div>
+    </section>
   );
 };
 
