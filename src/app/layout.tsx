550412629951923f7ec34e6b72e64784d1f51919
Royer Adames <royeraadames@gmail.com>
2025-09-30 15:32:00 -0400

    add the provider and so that the sidebar loads, and default to close and show the icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import Header from "@/components/header";
-import { Sidebar } from "@/components/sidebar/components/sidebar";
+import { Sidebar } from "@/components/sidebar/sidebar";
 import { Toaster } from "@/components/sonner";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -32,20 +33,22 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider>
-          <Header />
-          <div className="flex h-screen overflow-hidden border-collapse">
-            <Sidebar />
-            <main className="duration-200 pl-[100px] peer-hover:pl-[240px] min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
-              {children}
-            </main>
-          </div>
-          <Toaster expand />
-          <footer>
-            <p>
-              &copy; {new Date().getFullYear()} The Road to Next. All rights
-              reserved.
-            </p>
-          </footer>
+          <SidebarProvider className="flex-col" defaultOpen={false}>
+            <Header />
+            <div className="flex h-screen overflow-hidden border-collapse">
+              <Sidebar />
+              <main className="min-h-screen flex-1 overflow-y-auto overflow-x-hidden py-24 px-8 bg-secondary/20 flex flex-col">
+                {children}
+              </main>
+            </div>
+            <Toaster expand />
+            <footer>
+              <p>
+                &copy; {new Date().getFullYear()} The Road to Next. All rights
+                reserved.
+              </p>
+            </footer>
+          </SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
