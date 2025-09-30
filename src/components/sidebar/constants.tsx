ba9d517120aeb081effebd3c0ba4bc402705ebac
Royer Adames <royeraadames@gmail.com>
2025-09-30 15:31:32 -0400

    update sizebar and sidebar item to use shadcn sidebar

--- src/components/sidebar/constants.tsx
+++ src/components/sidebar/constants.tsx
@@ -20,6 +20,3 @@ export const navItems: NavItem[] = [
     href: accountProfilePath(),
   },
 ];
-
-export const closedClassName =
-  "text-background opacity-0 transition-all duration-300 group-hover:z-40 group-hover:ml-4 group-hover:rounded group-hover:bg-foreground group-hover:p-2 group-hover:opacity-100";
