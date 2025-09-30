1cd0dadfad9011290777df893af186817821f858
Royer Adames <royeraadames@gmail.com>
2025-09-30 13:10:35 -0400

    place form on page

--- src/app/account/profile/page.tsx
+++ src/app/account/profile/page.tsx
@@ -1,15 +1,22 @@
 import Heading from "@/components/heading";
 import { AccountTabs } from "@/features/account/components/account-tabs";
+import { UserUpdateForm } from "@/features/auth/component/user-update-form";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
 
-const profilePage = () => {
+const profilePage = async () => {
+  const { user: authUser } = await getAuthOrRedirect();
+
   return (
-    <div className="flex-1 flex flex-col gap-y-8">
+    <section className="flex-1 flex flex-col gap-y-8">
       <Heading
         title="Profile"
         description="All your profile information"
         tabs={<AccountTabs />}
       />
-    </div>
+      <div className="w-full max-w-[420px] self-center">
+        <UserUpdateForm lastName={authUser.lastName} firstName={authUser.firstName} username={authUser.username} />
+      </div>
+    </section>
   );
 };
 
