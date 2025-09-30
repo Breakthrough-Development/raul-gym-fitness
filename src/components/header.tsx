f6312b6cf6b9e62b5f55b20d6073de816c460d06
Royer Adames <royeraadames@gmail.com>
2025-09-30 17:19:28 -0400

    set it the colors to work with the theme

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,6 +6,7 @@ import { LucideKanban } from "lucide-react";
 import Link from "next/link";
 import { AccountDropdown } from "./account-dropdown";
 import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
+import { ScrollProgress } from "./ui/scroll-progress";
 import { SidebarTrigger } from "./ui/sidebar";
 
 const Header = () => {
@@ -41,12 +42,15 @@ const Header = () => {
   );
   return (
     <header>
+      <ScrollProgress className="top-[58px] bg-none dark:bg-white bg-black" />
       <nav>
         <ul className="animate-header-from-top supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex py-2.5 px-5 justify-between">
           <div className="flex items-center gap-2">
-            <li>
-              <SidebarTrigger className="cursor-pointer" />
-            </li>
+            {!!user && (
+              <li>
+                <SidebarTrigger className="cursor-pointer" />
+              </li>
+            )}
             <li>
               <Button asChild variant="ghost">
                 <Link href={homePath()}>
