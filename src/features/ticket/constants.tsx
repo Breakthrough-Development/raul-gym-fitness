6277d000c431a7b2a4c141136e00f9575070c4fb
Royer Adames <royeraadames@gmail.com>
2025-10-02 01:32:44 -0400

    adding sorting list

--- src/features/ticket/constants.tsx
+++ src/features/ticket/constants.tsx
@@ -11,3 +11,28 @@ export const TICKET_STATUS_LABELS = {
   IN_PROGRESS: "In Progress",
   DONE: "Done",
 } as const;
+
+export const SORT = {
+  NEWEST: "newest",
+  OLDEST: "oldest",
+  STATUS_OPEN: "status-open",
+  STATUS_IN_PROGRESS: "status-in-progress",
+  STATUS_DONE: "status-done",
+  BOUNTY_ASC: "bounty-asc",
+  BOUNTY_DESC: "bounty-desc",
+  TITLE_ASC: "title-asc",
+  TITLE_DESC: "title-desc",
+} as const;
+
+export const SORT_OPTIONS = [
+  { value: SORT.NEWEST, label: "Newest" },
+  { value: SORT.OLDEST, label: "Oldest" },
+  { value: SORT.STATUS_OPEN, label: "Status: Open" },
+  { value: SORT.STATUS_IN_PROGRESS, label: "Status: In Progress" },
+  { value: SORT.STATUS_DONE, label: "Status: Done" },
+  { value: SORT.BOUNTY_ASC, label: "Lowest Bounty" },
+  { value: SORT.BOUNTY_DESC, label: "Highest Bounty" },
+  { value: SORT.TITLE_ASC, label: "A-Z" },
+  { value: SORT.TITLE_DESC, label: "Z-A" },
+];
+
