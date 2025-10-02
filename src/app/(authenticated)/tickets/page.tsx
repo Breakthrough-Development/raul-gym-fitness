5d3dff61a23620cd88ff148fd968ba8559cd734b
Royer Adames <royeraadames@gmail.com>
2025-10-01 23:55:57 -0400

    update the page ticket list to passdown the search params

--- src/app/(authenticated)/tickets/page.tsx
+++ src/app/(authenticated)/tickets/page.tsx
@@ -1,14 +1,19 @@
+import { CardComp } from "@/components/card-comp";
 import Heading from "@/components/heading";
-import TicketList from "@/features/ticket/components/ticket-list";
+import Placeholder from "@/components/placeholder";
 import { Spinner } from "@/components/spiner";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import TicketList from "@/features/ticket/components/ticket-list";
+import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
+import { SearchParams } from "@/features/ticket/search-params";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import Placeholder from "@/components/placeholder";
-import { CardComp } from "@/components/card-comp";
-import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
-import { getAuth } from "@/features/auth/queries/get-auth";
 
-export default async function TicketsPage() {
+type TicketsPageProps = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function TicketsPage({ searchParams }: TicketsPageProps) {
   const { user } = await getAuth();
   return (
     <section className="flex-1 flex flex-col gap-y-8">
@@ -26,7 +31,7 @@ export default async function TicketsPage() {
 
       <ErrorBoundary fallback={<Placeholder label="Error loading tickets" />}>
         <Suspense fallback={<Spinner />}>
-          <TicketList userId={user?.id} />
+          <TicketList userId={user?.id} searchParams={await searchParams} />
         </Suspense>
       </ErrorBoundary>
     </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 import Heading from "@/components/heading";
 import { Spinner } from "@/components/spiner";
 import TicketList from "@/features/ticket/components/ticket-list";
+import { SearchParams } from "@/features/ticket/search-params";
 import { Suspense } from "react";
 
-export default function HomePage() {
+type HomePageProps = {
+  searchParams: Promise<SearchParams>;
+};
+
+export default async function HomePage({ searchParams }: HomePageProps) {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Home" description="Your home place to start" />
 
       <Suspense fallback={<Spinner />}>
-        <TicketList />
+        <TicketList searchParams={await searchParams} />
       </Suspense>
     </div>
   );
