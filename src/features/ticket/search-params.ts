e3159c40f5f53255bc9e7121f2acf5f173855977
Royer Adames <royeraadames@gmail.com>
2025-10-04 13:33:36 -0400

    have sortParse be a object of the option; put the option in the url

--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -1,19 +1,25 @@
 import { createSearchParamsCache, parseAsString } from "nuqs/server";
-import { SORT } from "./constants";
+import { SORT_LABELS, SORT_VALUES, TICKET_KEYS } from "./constants";
 
 export const searchParser = parseAsString.withDefault("").withOptions({
   shallow: false,
   clearOnDefault: true,
 });
 
-export const sortParser = parseAsString.withDefault(SORT.NEWEST).withOptions({
+export const sortParser = {
+  sortKey: parseAsString.withDefault(TICKET_KEYS.CREATED_AT),
+  sortValue: parseAsString.withDefault(SORT_VALUES.DESC),
+  sortLabel: parseAsString.withDefault(SORT_LABELS.NEWEST),
+};
+
+export const sortOptions = {
   shallow: false,
   clearOnDefault: true,
-});
+};
 
 export const SearchParamsCache = createSearchParamsCache({
   search: searchParser,
-  sort: sortParser,
+  ...sortParser,
 });
 
 export type ParsedSearchParams = Awaited<
