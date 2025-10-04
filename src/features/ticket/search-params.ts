5b193429f2cbe9c7a3ec097e0d2cab15450bb7d2
Royer Adames <royeraadames@gmail.com>
2025-10-04 06:49:01 -0400

    refactor search-params to introduce dedicated parsers for search and sort, enhancing parameter handling

--- src/features/ticket/search-params.ts
+++ src/features/ticket/search-params.ts
@@ -1,8 +1,19 @@
 import { createSearchParamsCache, parseAsString } from "nuqs/server";
+import { SORT } from "./constants";
+
+export const searchParser = parseAsString.withDefault("").withOptions({
+  shallow: false,
+  clearOnDefault: true,
+});
+
+export const sortParser = parseAsString.withDefault(SORT.NEWEST).withOptions({
+  shallow: false,
+  clearOnDefault: true,
+});
 
 export const SearchParamsCache = createSearchParamsCache({
-  search: parseAsString.withDefault(""),
-  sort: parseAsString.withDefault(""),
+  search: searchParser,
+  sort: sortParser,
 });
 
 export type ParsedSearchParams = Awaited<
