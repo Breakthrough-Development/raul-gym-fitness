b3b731cfa2ca110050d5cf31842e373fd9de6c5b
Royer Adames <royeraadames@gmail.com>
2025-10-04 18:29:19 -0400

    Add TicketPagination component for managing ticket pagination

---
+++ src/features/ticket/components/ticket-pagination.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Pagination } from "@/components/pagination";
+import { useQueryStates } from "nuqs";
+import { paginationOptions, paginationParser } from "../search-params";
+
+export const TicketPagination = () => {
+  const [pagination, setPagination] = useQueryStates(
+    paginationParser,
+    paginationOptions
+  );
+  return <Pagination pagination={pagination} onPageAndSize={setPagination} />;
+};
