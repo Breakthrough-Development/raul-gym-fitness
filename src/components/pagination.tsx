5b362cb1f24eff71e2e33d8de28b0562bc333b7e
Royer Adames <royeraadames@gmail.com>
2025-10-04 22:38:44 -0400

    Add size selection to Pagination component for customizable page size

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { PAGINATION_SIZE_OPTIONS } from "@/features/ticket/constants";
 
 type PageAndSize = {
   page: number;
@@ -54,10 +62,33 @@ export const Pagination = ({
       Next
     </Button>
   );
+
+  const handleChangeSize = (size: string) => {
+    onPageAndSize({ page: 0, size: parseInt(size) });
+  };
+
+  const sizeButton = (
+    <Select
+      defaultValue={pagination.size.toString()}
+      onValueChange={handleChangeSize}
+    >
+      <SelectTrigger className="h-[36px]">
+        <SelectValue placeholder="Size" />
+      </SelectTrigger>
+      <SelectContent>
+        {PAGINATION_SIZE_OPTIONS.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
   return (
     <div className="flex justify-between items-center">
       <p className="text-sm text-muted-foreground">{label}</p>
       <div className="flex gap-x-2">
+        {sizeButton}
         <PreviousButton />
         <NextButton />
       </div>
