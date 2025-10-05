683b77a3e88230b54c7fdcba31766ec6f523d019
Royer Adames <royeraadames@gmail.com>
2025-10-05 00:23:47 -0400

    Refactor GET route to accept search parameters from request, enhancing ticket fetching capabilities

--- src/app/api/tickets/route.ts
+++ src/app/api/tickets/route.ts
@@ -1,13 +1,12 @@
 import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { SearchParamsCache } from "@/features/ticket/search-params";
 
-export async function GET() {
-  const { list, metadata } = await getTickets(undefined, {
-    search: "",
-    size: 5,
-    page: 0,
-    sortKey: "createdAt",
-    sortValue: "desc",
-  });
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  const untypedSearchParams = Object.fromEntries(searchParams);
+  const typedSearchParams = SearchParamsCache.parse(untypedSearchParams);
+  const { list, metadata } = await getTickets(undefined, typedSearchParams);
 
   return Response.json({ list, metadata });
 }
