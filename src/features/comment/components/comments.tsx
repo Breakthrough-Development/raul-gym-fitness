d99e3558724f6bc0ca313e9e97dc3a76a6864d60
Royer Adames <royeraadames@gmail.com>
2025-10-07 18:12:40 -0400

    Refactor Comments component to fetch comments based on ticketId and integrate CommentCreateForm for new comment submissions

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,19 +1,29 @@
-"use client";
-import { CommetWithMetaData } from "../types";
+import { CardComp } from "@/components/card-comp";
+import { getComments } from "../queries/get-comments";
+import { CommentCreateForm } from "./comment-create-form";
 import { CommentItem } from "./comment-item";
 
 type CommentsProps = {
-  comments: CommetWithMetaData[];
+  ticketId: string;
 };
 
-export const Comments = ({ comments }: CommentsProps) => {
+export const Comments = async ({ ticketId }: CommentsProps) => {
+  const comments = await getComments(ticketId);
+
   return (
-    <ul className="flex flex-col gap-y-2 ml-8">
-      {comments.map((comment) => (
-        <li key={comment.id} className="w-full">
-          <CommentItem comment={comment} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <CardComp
+        title="Comment"
+        description="A comment on a ticket"
+        content={<CommentCreateForm ticketId={ticketId} />}
+      />
+      <ul className="flex flex-col gap-y-2 ml-8">
+        {comments.map((comment) => (
+          <li key={comment.id} className="w-full">
+            <CommentItem comment={comment} />
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
