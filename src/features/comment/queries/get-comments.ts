0b13b23d62d8cbbfbb5e751ecae32cac51db7d40
Royer Adames <royeraadames@gmail.com>
2025-10-07 15:27:11 -0400

    Add getComments query to fetch comments for a specific ticket, including user details and ordered by creation date

---
+++ src/features/comment/queries/get-comments.ts
@@ -0,0 +1,18 @@
+"use server";
+import { prisma } from "@/lib/prisma";
+
+export const getComments = async (ticketId: string) => {
+  return await prisma.comment.findMany({
+    where: { ticketId },
+    include: {
+      user: {
+        select: {
+          username: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
