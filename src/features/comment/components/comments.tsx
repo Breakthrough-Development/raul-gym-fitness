7329aada309cfb45880e5c5bdd496b7f9c7f343d
Royer Adames <royeraadames@gmail.com>
2025-10-07 19:17:16 -0400

    Integrate user authentication into Comments component and conditionally render delete button for comment owners

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -1,6 +1,9 @@
 import { CardComp } from "@/components/card-comp";
+import { getAuth } from "@/features/auth/queries/get-auth";
+import { isOwner } from "@/features/auth/utils/is-owner";
 import { getComments } from "../queries/get-comments";
 import { CommentCreateForm } from "./comment-create-form";
+import { CommentDeleteButton } from "./comment-delete-button";
 import { CommentItem } from "./comment-item";
 
 type CommentsProps = {
@@ -9,6 +12,7 @@ type CommentsProps = {
 
 export const Comments = async ({ ticketId }: CommentsProps) => {
   const comments = await getComments(ticketId);
+  const { user } = await getAuth();
 
   return (
     <>
@@ -20,7 +24,14 @@ export const Comments = async ({ ticketId }: CommentsProps) => {
       <ul className="flex flex-col gap-y-2 ml-8">
         {comments.map((comment) => (
           <li key={comment.id} className="w-full">
-            <CommentItem comment={comment} />
+            <CommentItem
+              comment={comment}
+              buttons={[
+                ...(isOwner(user, comment)
+                  ? [<CommentDeleteButton key="0" id={comment.id} />]
+                  : []),
+              ]}
+            />
           </li>
         ))}
       </ul>
