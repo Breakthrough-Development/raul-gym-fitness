37573e4fd7422ac3394f0ae5943e439c2e91a678
Royer Adames <royeraadames@gmail.com>
2025-10-10 06:57:49 -0400

    remove mock waiting

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,8 +1,6 @@
 import { prisma } from "@/lib/prisma";
 
 const getTicket = async function getTicket(id: string) {
-  console.log("ticket")
-  await new Promise((resolve) => setTimeout(resolve, 2000));
   return await prisma.ticket.findUnique({
     where: {
       id,
