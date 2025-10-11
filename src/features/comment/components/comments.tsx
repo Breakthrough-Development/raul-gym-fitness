ca24e962aad0d337b8f5275cbe8bc07d361fcd0e
Royer Adames <royeraadames@gmail.com>
2025-10-11 00:14:23 -0400

    Remove TODO comment regarding repeated comments in Comments component for cleaner code.

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -35,7 +35,6 @@ export const Comments = ({ ticketId, paginatedComments }: CommentsProps) => {
       />
       <ul className="flex flex-col gap-y-2 ml-8">
         {comments.map((comment) => (
-          // TODO: fix bug, getting repeated comments
           <li key={comment.id} className="w-full">
             <CommentItem
               comment={comment}
