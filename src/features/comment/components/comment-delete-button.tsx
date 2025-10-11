f9e94bc77a10cda80904e93858a8e9d36a135605
Royer Adames <royeraadames@gmail.com>
2025-10-11 14:46:13 -0400

    is the callback loading flag for adding a loading UI state to the delete button

--- src/features/comment/components/comment-delete-button.tsx
+++ src/features/comment/components/comment-delete-button.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useConfirmDialog } from "@/components/confirm-dialog";
 import { Button } from "@/components/ui/button";
-import { LucideTrash } from "lucide-react";
+import { LucideLoaderCircle, LucideTrash } from "lucide-react";
 import { deleteComment } from "../actions/delete-comment";
 
 type CommentDeleteButtonType = {
@@ -15,9 +15,13 @@ export const CommentDeleteButton = ({
 }: CommentDeleteButtonType) => {
   const [deleteButton, deleteDialog] = useConfirmDialog({
     action: deleteComment.bind(null, id),
-    trigger: (
+    trigger: (isLoading) => (
       <Button variant="destructive">
-        <LucideTrash className="h-4 w-4" />
+        {isLoading ? (
+          <LucideLoaderCircle className="h-4 w-4 animate-spin" />
+        ) : (
+          <LucideTrash className="h-4 w-4" />
+        )}
         <span className="sr-only">Delete Comment</span>
       </Button>
     ),
