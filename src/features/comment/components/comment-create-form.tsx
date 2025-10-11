f80ac43da7f44b1a91779c9360ed90eee97c9459
Royer Adames <royeraadames@gmail.com>
2025-10-11 00:43:11 -0400

    Enhance CommentCreateForm to support onCreateComment callback for improved comment handling after creation. Update form submission to utilize ActionState for success feedback.

--- src/features/comment/components/comment-create-form.tsx
+++ src/features/comment/components/comment-create-form.tsx
@@ -2,23 +2,35 @@
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
-import { EMPTY_ACTION_STATE } from "@/components/form/util/to-action-state";
+import {
+  ActionState,
+  EMPTY_ACTION_STATE,
+} from "@/components/form/util/to-action-state";
 import { Textarea } from "@/components/ui/textarea";
 import { useActionState } from "react";
 import { createComment } from "../actions/create-comment";
+import { CommentWithMetaData } from "../types";
 
 type CommentCreateForm = {
   ticketId: string;
+  onCreateComment?: (comment: CommentWithMetaData | undefined) => void;
 };
 
-export const CommentCreateForm = ({ ticketId }: CommentCreateForm) => {
+export const CommentCreateForm = ({
+  ticketId,
+  onCreateComment,
+}: CommentCreateForm) => {
   const [actionState, action] = useActionState(
     createComment.bind(null, ticketId),
     EMPTY_ACTION_STATE
   );
 
+  const handleSuccess = (actionState: ActionState) => {
+    onCreateComment?.(actionState.data as CommentWithMetaData);
+  };
+
   return (
-    <Form action={action} actionState={actionState}>
+    <Form action={action} actionState={actionState} onSuccess={handleSuccess}>
       <Textarea name="content" placeholder="What's on your mind ..." />
       <FieldError actionState={actionState} name="content" />
       <SubmitButton label="Create Comment" />
