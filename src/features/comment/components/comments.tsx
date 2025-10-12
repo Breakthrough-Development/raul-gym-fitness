4efd336ca78646f139caabde8358a898b7bb3f3e
Royer Adames <royeraadames@gmail.com>
2025-10-12 01:24:55 -0400

    simple way to update the client UI once the db is updated

--- src/features/comment/components/comments.tsx
+++ src/features/comment/components/comments.tsx
@@ -16,7 +16,7 @@ type CommentsProps = {
 };
 
 export const Comments = ({ ticketId, paginatedComments }: CommentsProps) => {
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, refetch } =
     useInfiniteQuery({
       queryKey: ["comments", ticketId],
       queryFn: ({ pageParam }) => getComments(ticketId, pageParam),
@@ -38,12 +38,8 @@ export const Comments = ({ ticketId, paginatedComments }: CommentsProps) => {
   const handleMore = () => fetchNextPage();
   const comments = data.pages.flatMap((page) => page.list);
 
-  const handleDeleteComment = (id: string) => {
-    // TODO
-  };
-  const handleCreateComment = (comment: CommentWithMetadata | undefined) => {
-    // TODO
-  };
+  const handleDeleteComment = () => refetch();
+  const handleCreateComment = () => refetch();
   return (
     <>
       <CardComp
