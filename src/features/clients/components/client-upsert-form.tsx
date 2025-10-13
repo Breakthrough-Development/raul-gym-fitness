9d1dd33938f4e05dab455e54daaa301c6cd351aa
Royer Adames <royeraadames@gmail.com>
2025-10-13 01:42:35 -0400

    Add ClientUpsertForm component for creating and editing client details; includes form fields for first name, last name, phone, and email with error handling and submission functionality.

---
+++ src/features/clients/components/client-upsert-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { FieldError } from "@/components/form/field-error";
+import { Form } from "@/components/form/form";
+import { SubmitButton } from "@/components/form/submit-button";
+import { EMPTY_ACTION_STATE } from "@/components/form/util/to-action-state";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Client } from "@prisma/client";
+import { useActionState } from "react";
+import { upsertClient } from "../actions/upsert-client";
+
+export type ClientUpsertFormProps = {
+  client?: Client;
+};
+
+export const ClientUpsertForm = ({ client }: ClientUpsertFormProps) => {
+  const [actionState, formAction] = useActionState(
+    upsertClient.bind(null, client?.id),
+    EMPTY_ACTION_STATE
+  );
+
+  return (
+    <Form action={formAction} actionState={actionState}>
+      <Label htmlFor="firstName">First Name</Label>
+      <Input
+        id="firstName"
+        name="firstName"
+        type="text"
+        defaultValue={
+          (actionState.payload?.get("firstName") as string) ?? client?.firstName
+        }
+      />
+      <FieldError actionState={actionState} name="firstName" />
+
+      <Label htmlFor="lastName">Last Name</Label>
+      <Input
+        id="lastName"
+        name="lastName"
+        defaultValue={
+          (actionState.payload?.get("lastName") as string) ?? client?.lastName
+        }
+      />
+      <FieldError actionState={actionState} name="lastName" />
+
+      <Label htmlFor="phone">Phone</Label>
+      <Input
+        id="phone"
+        name="phone"
+        defaultValue={
+          (actionState.payload?.get("phone") as string) ?? client?.phone
+        }
+      />
+      <FieldError actionState={actionState} name="phone" />
+
+      <Label htmlFor="email">Email</Label>
+      <Input
+        id="email"
+        name="email"
+        defaultValue={
+          (actionState.payload?.get("email") as string) ?? client?.email
+        }
+      />
+      <FieldError actionState={actionState} name="email" />
+
+      <SubmitButton label={client ? "Edit" : "Create"} />
+
+      {actionState.message}
+    </Form>
+  );
+};
