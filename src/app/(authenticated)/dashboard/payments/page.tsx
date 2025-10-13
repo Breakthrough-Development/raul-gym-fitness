73168a5c69a64bbb646484236a5f7ae51f8b1a04
Royer Adames <royeraadames@gmail.com>
2025-10-12 22:30:11 -0400

    Add profile page components for user information update. This includes the integration of AccountTabs, Heading, and UserUpdateForm, along with authentication handling via getAuthOrRedirect.

---
+++ src/app/(authenticated)/dashboard/payments/page.tsx
@@ -0,0 +1,27 @@
+import { AccountTabs } from "@/app/(authenticated)/account/_navigation/account-tabs";
+import Heading from "@/components/heading";
+import { UserUpdateForm } from "@/features/auth/component/user-update-form";
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+
+const profilePage = async () => {
+  const { user: authUser } = await getAuthOrRedirect();
+
+  return (
+    <section className="flex-1 flex flex-col gap-y-8">
+      <Heading
+        title="Profile"
+        description="All your profile information"
+        tabs={<AccountTabs />}
+      />
+      <div className="w-full max-w-[420px] self-center">
+        <UserUpdateForm
+          lastName={authUser.lastName}
+          firstName={authUser.firstName}
+          username={authUser.username}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default profilePage;
