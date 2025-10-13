a4f0421c77abe4e983586f705e4a7905538af793
Royer Adames <royeraadames@gmail.com>
2025-10-13 01:42:31 -0400

    Add ClientList component to display and manage client listings; includes search, sorting, and pagination features.

---
+++ src/features/clients/components/client-list.tsx
@@ -0,0 +1,37 @@
+import Placeholder from "@/components/placeholder";
+import { TicketPagination } from "@/features/ticket/components/ticket-pagination";
+import { TicketSearchInput } from "@/features/ticket/components/ticket-search-input";
+import { TicketSortSelect } from "@/features/ticket/components/ticket-sort-select";
+import { ParsedSearchParams } from "@/features/ticket/search-params";
+import { SORT_OPTIONS } from "../constants";
+import { getClients } from "../queries/get-actions";
+import { ClientItem } from "./client-item";
+
+export type ClientListProps = {
+  searchParams: ParsedSearchParams;
+};
+
+export async function ClientList({ searchParams }: ClientListProps) {
+  const { list: clients, metadata } = await getClients(searchParams);
+  return (
+    <section className="flex flex-col gap-y-4 animate-fade-from-top">
+      <header className="self-center max-w-[420px] w-full flex gap-x-2">
+        <h2 className="sr-only">Clients</h2>
+        <TicketSearchInput placeholder="Search clients by name" />
+        <TicketSortSelect options={SORT_OPTIONS} />
+      </header>
+      <ul className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
+        {clients.length ? (
+          clients.map((client) => (
+            <ClientItem key={client.id} client={client} />
+          ))
+        ) : (
+          <Placeholder label="No clients found" />
+        )}
+      </ul>
+      <footer className="w-full max-w-[420px] self-center">
+        <TicketPagination paginatedMetaData={metadata} />
+      </footer>
+    </section>
+  );
+}
