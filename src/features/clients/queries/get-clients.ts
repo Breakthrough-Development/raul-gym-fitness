c593308531b2176a2b7bccffbff25c4b22566c59
Royer Adames <royeraadames@gmail.com>
2025-10-13 01:55:16 -0400

    Add getClients query to retrieve client data with search, pagination, and sorting; includes authentication and payment information in the response.

---
+++ src/features/clients/queries/get-clients.ts
@@ -0,0 +1,70 @@
+import { getAuthOrRedirect } from "@/features/auth/queries/get-auth-or-redirect";
+import { ParsedSearchParams } from "@/features/ticket/search-params";
+import { prisma } from "@/lib/prisma";
+
+export const getClients = async (searchParams: ParsedSearchParams) => {
+  await getAuthOrRedirect();
+
+  const searchTerms = searchParams.search.trim().split(/\s+/);
+  const where =
+    searchTerms.length === 1
+      ? {
+          OR: [
+            {
+              firstName: {
+                contains: searchTerms[0],
+                mode: "insensitive" as const,
+              },
+            },
+            {
+              lastName: {
+                contains: searchTerms[0],
+                mode: "insensitive" as const,
+              },
+            },
+          ],
+        }
+      : {
+          AND: [
+            {
+              firstName: {
+                contains: searchTerms[0],
+                mode: "insensitive" as const,
+              },
+            },
+            {
+              lastName: {
+                contains: searchTerms[1],
+                mode: "insensitive" as const,
+              },
+            },
+          ],
+        };
+  const skip = searchParams.size * searchParams.page;
+  const take = searchParams.size;
+
+  const [clients, count] = await prisma.$transaction([
+    prisma.client.findMany({
+      where,
+      skip,
+      take,
+      orderBy: {
+        [searchParams.sortKey]: searchParams.sortValue,
+      },
+      include: {
+        Payment: true,
+      },
+    }),
+    prisma.client.count({
+      where,
+    }),
+  ]);
+
+  return {
+    list: clients,
+    metadata: {
+      count,
+      hasNextPage: count > skip + take,
+    },
+  };
+};
