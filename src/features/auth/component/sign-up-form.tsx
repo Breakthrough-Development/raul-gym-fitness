be96183e838a3f42c402625a3087b38ee9e59cf7
Royer Adames <royeraadames@gmail.com>
2025-10-18 17:57:18 -0400

    phone input and secret frace require before creating an account

--- src/features/auth/component/sign-up-form.tsx
+++ src/features/auth/component/sign-up-form.tsx
@@ -5,61 +5,88 @@ import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/util/to-action-state";
 import { PasswordField } from "@/components/password-field";
+import { PhoneInput } from "@/components/phone-input";
 import { Input } from "@/components/ui/input";
-import { useActionState } from "react";
+import { Label } from "@/components/ui/label";
+import { useActionState, useState } from "react";
 import { signUp } from "../actions/sign-up";
 
 const SignUpForm = () => {
   const [actionState, action] = useActionState(signUp, EMPTY_ACTION_STATE);
-
+  const [secretPhrase, setSecretPhrase] = useState("");
+  const requiredSecret = process.env.NEXT_PUBLIC_SECRET_FRASE ?? "";
   return (
-    <Form action={action} actionState={actionState}>
-      <Input
-        name="username"
-        placeholder="Nombre de usuario"
-        defaultValue={actionState.payload?.get("username") as string}
-      />
-      <FieldError actionState={actionState} name="username" />
+    <div>
+      <Form action={action} actionState={actionState} className=" gap-y-8">
+        <div className="flex flex-col gap-y-2">
+          <Label htmlFor={"frase-secreta"}>Por favor ingrese la frase secreta</Label>
+          <Input
+            id={"frase-secreta"}
+            name={"frase-secreta"}
+            placeholder="Por favor ingrese la frase secreta"
+            value={secretPhrase}
+            onChange={(e) => setSecretPhrase(e.target.value)}
+          />
+          <FieldError actionState={actionState} name="frase-secreta" />
+        </div>
+        <fieldset
+          disabled={secretPhrase !== requiredSecret}
+          className="flex flex-col gap-y-2"
+        > 
+          <Input
+            name="username"
+            placeholder="Nombre de usuario"
+            defaultValue={actionState.payload?.get("username") as string}
+          />
+          <FieldError actionState={actionState} name="username" />
 
-      <Input
-        name="firstName"
-        placeholder="Nombre"
-        defaultValue={actionState.payload?.get("firstName") as string}
-      />
-      <FieldError actionState={actionState} name="firstName" />
+          <Input
+            name="firstName"
+            placeholder="Nombre"
+            defaultValue={actionState.payload?.get("firstName") as string}
+          />
+          <FieldError actionState={actionState} name="firstName" />
 
-      <Input
-        name="lastName"
-        placeholder="Apellido"
-        defaultValue={actionState.payload?.get("lastName") as string}
-      />
-      <FieldError actionState={actionState} name="lastName" />
+          <Input
+            name="lastName"
+            placeholder="Apellido"
+            defaultValue={actionState.payload?.get("lastName") as string}
+          />
+          <FieldError actionState={actionState} name="lastName" />
 
-      <Input
-        name="email"
-        placeholder="Correo electrónico"
-        defaultValue={actionState.payload?.get("email") as string}
-      />
-      <FieldError actionState={actionState} name="email" />
+          <Input
+            name="email"
+            placeholder="Correo electrónico"
+            defaultValue={actionState.payload?.get("email") as string}
+          />
+          <FieldError actionState={actionState} name="email" />
 
-      <PasswordField
-        type="password"
-        name="password"
-        placeholder="Contraseña"
-        defaultValue={actionState.payload?.get("password") as string}
-      />
-      <FieldError actionState={actionState} name="password" />
+          <PhoneInput
+            actionState={actionState}
+            name="phone"
+            defaultValue={actionState.payload?.get("phone") as string}
+          />
+          <FieldError actionState={actionState} name="phone" />
+          <PasswordField
+            type="password"
+            name="password"
+            placeholder="Contraseña"
+            defaultValue={actionState.payload?.get("password") as string}
+          />
+          <FieldError actionState={actionState} name="password" />
 
-      <PasswordField
-        type="password"
-        name="confirmPassword"
-        placeholder="Confirmar contraseña"
-        defaultValue={actionState.payload?.get("confirmPassword") as string}
-      />
-      <FieldError actionState={actionState} name="confirmPassword" />
+          <PasswordField
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirmar contraseña"
+            defaultValue={actionState.payload?.get("confirmPassword") as string}
+          />
+          <FieldError actionState={actionState} name="confirmPassword" />
 
-      <SubmitButton label="Registrarse" />
-    </Form>
+          <SubmitButton label="Registrarse" />
+        </fieldset>
+      </Form>
+    </div>
   );
 };
 
