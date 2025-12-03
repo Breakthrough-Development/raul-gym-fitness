35dffa347985c3dc7ecf6d5bcdbe8c7f0b4d9b62
Royer Adames <raadamesbonilla@pplweb.com>
2025-12-02 22:49:03 -0400

    chore: Update dependencies in bun.lock, package.json, and next.config.ts to include babel-plugin-react-compiler and enable experimental features for improved performance and development experience.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactCompiler: true,
   experimental: {
     typedRoutes: true,
+    turbopackFileSystemCacheForDev: true,
   },
 };
 
